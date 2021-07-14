773c324415a774b2dd9832f713d991ea52dcf7de
sunjung <sunjung@adop.cc>
2021-07-15 00:04:33 +0900

    flag set

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-	"github.com/gguldduck111/nomadcoin/rest"
+	"flag"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to 노마드 코인\n\n")
+	fmt.Printf("Please use the following commands\n\n")
+	fmt.Printf("explorer: 	Start the HTML Explorer\n")
+	fmt.Printf("rest: 		Start the REST API (recommended)\n")
+	os.Exit(0)
+}
+
 func main() {
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+	portFlag := rest.Int("port", 4000, "Sets the port of the server ")
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		rest.Parse(os.Args[2:])
+	default:
+		usage()
+	}
+
+	if rest.Parsed() {
+		fmt.Println(*portFlag)
+		fmt.Println("Start Server")
+	}
 }
